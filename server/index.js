21614e1cc6f13695f4321f680627eec5a2a2ce22
gbalki <serhatbalki97@gmail.com>
2025-09-05 23:42:14 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/my-blog-app/contact/sendEmail', async (req, res) => {
+app.post('/sendEmail', async (req, res) => {
     const { name, email, message } = req.body;
 
     try {
